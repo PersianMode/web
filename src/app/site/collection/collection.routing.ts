6a2168ec9e95227202aa04426adc289f75e6bf42
arhariri <ali.71hariri@gmail.com>
2018-01-29 17:41:57 +0330

    add 5 optional parameters to routes to collection module some improvement on header

--- src/app/site/collection/collection.routing.ts
+++ src/app/site/collection/collection.routing.ts
@@ -3,7 +3,12 @@ import {RouterTestingModule} from '@angular/router/testing';
 import {MainCollectionComponent} from './components/main-collection/main-collection.component';
 
 const Collection_ROUTES: Routes = [
-  {path: ':name', component: MainCollectionComponent}
+  {path: ':typeName', component: MainCollectionComponent},
+  {path: ':typeName/:l1', component: MainCollectionComponent},
+  {path: ':typeName/:l1/:l2', component: MainCollectionComponent},
+  {path: ':typeName/:l1/:l2/:l3', component: MainCollectionComponent},
+  {path: ':typeName/:l1/:l2/:l3/:l4', component: MainCollectionComponent},
+  {path: ':typeName/:l1/:l2/:l3/:l4/:l5', component: MainCollectionComponent},
 ];
 
 export const CollectionRouting = RouterModule.forChild(Collection_ROUTES);
