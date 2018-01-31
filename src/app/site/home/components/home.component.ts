744c044defab8f1611bfbee7d42fbe7a733800e2
305-2 <saresalehi@yaoo.com>
2018-01-31 18:25:05 +0330

    commit

--- src/app/site/home/components/home.component.ts
+++ src/app/site/home/components/home.component.ts
@@ -9,103 +9,256 @@ export class HomeComponent implements OnInit {
   placements = {
     panels: [
       {
-        imgUrl: '../../../../assets/pictures/pw-1.png',
-        href: '#',
-        areas: [
-          {
-            pos: 'left-center',
-            title: 'متفاوت باش!',
-            text: 'حرکت رو به جلو ...',
-          },
-        ],
         type: 'full',
-      },
-      {
-        imgUrl: '../../../../assets/pictures/pw-2.png',
-        href: '#',
-        areas: [
+        topTitle: {
+          title: '',
+          text: '',
+          color: '',
+        },
+        subTitle: {
+          title: '',
+          text: '',
+          color: '',
+        },
+        imgs: [
           {
-            pos: 'right-center',
-            title: 'مثل همیشه، فراتر از زمان!',
-            text: 'معرفی محصولات جدید نایک پلاس',
-            color: 'black',
+            imgUrl: '../../../../assets/pictures/pw-1.png',
+            href: '#',
+            topTitle: {
+              title: '',
+              text: '',
+              color: '',
+              textColor: ''
+            },
+            subTitle: {
+              title: '',
+              text: '',
+              color: '',
+              textColor: '',
+            },
+            areas: [
+              {
+                pos: 'left-center',
+                title: 'متفاوت باش!',
+                text: 'حرکت رو به جلو ...',
+              },
+            ],
           },
-        ],
-        type: 'full',
-      },
-      {
-        imgUrl: '../../../../assets/pictures/pw-7.png',
-        href: '#',
-        areas: [
-          {
-            pos: 'left-center',
-            title: 'خاکستری بی نظیر!',
-            text: 'برای اولین بار',
-          },
-        ],
-        type: 'full',
-      },
-      {
-        imgUrl: '../../../../assets/pictures/pw-4.png',
-        href: '#',
-        areas: [
-          {
-            pos: 'left-center',
-            title: 'کاملا گرم',
-            text: 'محصولات ابریشمی مناسب زمستان',
-          },
-          {
-            pos: 'right-center',
-            title: 'زمان درخشیدن توست!',
-            text: 'نایک، حامی تیم ملی در طول بازیها',
-          },
-        ],
-        type: 'full',
-      },
-      {
-        imgUrl: '../../../../assets/pictures/pw-11.png',
-        href: '#',
-        areas: [
-          {
-            pos: 'right-top',
-            title: '',
-            text: 'رنگهای جدید، دلخواه شما',
-            color: 'black',
-          },
-        ],
-        type: 'quarter',
-        subTitle : [
-          {
-            pos : 'quarter1',
-            title: 'کفش راحتی زنانه، مدل ژاکلین',
-            text: 'کفش زنانه',
-            color: 'black',
-            textColor: 'gray'
-          },
-          {
-            pos : 'quarter2',
-            title: 'کفش کارمندی زنانه',
-            text: 'کفش زنانه',
-            color: 'black',
-            textColor: 'gray'
-          },
-          {
-            pos : 'quarter3',
-            title: 'کفش ورزشی زنانه نایک، سری نایک پلاس',
-            text: 'کفش ورزشی زنانه',
-            color: 'black',
-            textColor: 'gray'
-          },
-          {
-            pos : 'quarter4',
-            title: 'کفش پیاده روی زنانه نایک، مدل پگاسوس',
-            text: 'کفش پیاده روی زنانه',
-            color: 'black',
-            textColor: 'gray'
-          }
         ]
-      }
-    ],
+      },
+      {
+        type: 'full',
+        topTitle: {
+          title: '',
+          text: '',
+          color: '',
+        },
+        subTitle: {
+          title: '',
+          text: '',
+          color: '',
+        },
+        imgs: [
+          {
+            imgUrl: '../../../../assets/pictures/pm-2.png',
+            href: '#',
+            topTitle: {
+              title: '',
+              text: '',
+              color: '',
+              textColor: ''
+            },
+            subTitle: {
+              title: '',
+              text: '',
+              color: 'black',
+              textColor: 'gray',
+            },
+            areas: [
+              {
+                pos: 'right-center',
+                title: 'مثل همیشه، فراتر از زمان!',
+                text: 'معرفی محصولات جدید نایک پلاس',
+                color: 'black',
+              },
+            ],
+          },
+        ]
+      },
+      {
+        type: 'full',
+        topTitle: {
+          title: '',
+          text: '',
+          color: '',
+        },
+        subTitle: {
+          title: '',
+          text: '',
+          color: '',
+        },
+        imgs: [
+          {
+            imgUrl: '../../../../assets/pictures/pm-7.png',
+            href: '#',
+            topTitle: {
+              title: '',
+              text: '',
+              color: '',
+              textColor: ''
+            },
+            subTitle: {
+              title: '',
+              text: '',
+              color: 'black',
+              textColor: 'gray',
+            },
+            areas: [
+              {
+                pos: 'left-center',
+                title: 'خاکستری بی نظیر!',
+                text: 'برای اولین بار',
+              },
+            ],
+          },
+        ]
+      },
+      {
+        type: 'full',
+        topTitle: {
+          title: '',
+          text: '',
+          color: '',
+        },
+        subTitle: {
+          title: '',
+          text: '',
+          color: '',
+        },
+        imgs: [
+          {
+            imgUrl: '../../../../assets/pictures/pm-4.png',
+            href: '#',
+            topTitle: {
+              title: '',
+              text: '',
+              color: '',
+              textColor: ''
+            },
+            subTitle: {
+              title: '',
+              text: '',
+              color: '',
+              textColor: '',
+            },
+            areas: [
+              {
+                pos: 'left-center',
+                title: 'کاملا گرم',
+                text: 'محصولات پشمی مناسب زمستان',
+              },
+              {
+                pos: 'right-center',
+                title: 'زمان درخشیدن توست!',
+                text: 'نایک، حامی تیم ملی در طول بازیها',
+              },
+            ],
+          },
+        ]
+      },
+      {
+        type: 'quarter',
+        topTitle: {
+          title: 'رنگهای جدید، دلخواه شما',
+          text: '',
+          color: 'black',
+        },
+        subTitle: {
+          title: '',
+          text: '',
+          color: 'black',
+        },
+        imgs: [
+          {
+            imgUrl: '../../../../assets/pictures/q1.png',
+            href: '#',
+            topTitle: {
+              title: '',
+              text: '',
+              color: '',
+              textColor: ''
+            },
+            subTitle: {
+              title: 'کفش راحتی زنانه، مدل ژاکلین',
+              text: 'کفش زنانه',
+              color: 'black',
+              textColor: 'gray',
+            },
+            areas: [],
+          },
+          {
+            imgUrl: '../../../../assets/pictures/q2.png',
+            href: '#',
+            topTitle: {
+              title: '',
+              text: '',
+              color: '',
+              textColor: ''
+            },
+            subTitle: {
+              title: 'کفش کارمندی زنانه',
+              text: 'کفش زنانه',
+              color: 'black',
+              textColor: 'gray',
+            },
+            areas: [
+            ],
+          },
+          {
+            imgUrl: '../../../../assets/pictures/q3.png',
+            href: '#',
+            topTitle: {
+              title: '',
+              text: '',
+              color: '',
+              textColor: ''
+            },
+            subTitle: {
+              title: 'کفش ورزشی زنانه نایک، سری نایک پلاس',
+              text: 'کفش ورزشی زنانه',
+              color: 'black',
+              textColor: 'gray',
+            },
+            areas: [],
+          },
+          {
+            imgUrl: '../../../../assets/pictures/q4.png',
+            href: '#',
+            topTitle: {
+              title: '',
+              text: '',
+              color: '',
+              textColor: ''
+            },
+            subTitle: {
+              title: 'کفش پیاده روی زنانه نایک، مدل پگاسوس',
+              text: 'کفش پیاده روی زنانه',
+              color: 'black',
+              textColor: 'gray'
+            },
+            // areas: [{
+            //     pos: 'right-bottom',
+            //     title: 'خاکستری بی نظیر!',
+            //     text: 'برای اولین بار',
+            //     color: 'black',
+            //   },
+            // ],
+            areas: [
+            ],
+          },
+        ]
+      },
+    ]
   }
 
   constructor() { }
