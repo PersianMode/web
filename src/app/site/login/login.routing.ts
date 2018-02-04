bea7eeca3906622ce2b5cf47c57edda5b8b6185a
arhariri <ali.71hariri@gmail.com>
2018-02-04 14:25:14 +0330

    Add open authentication with google

--- src/app/site/login/login.routing.ts
+++ src/app/site/login/login.routing.ts
@@ -1,9 +1,11 @@
 import {RouterModule, Routes} from '@angular/router';
 import {RouterTestingModule} from '@angular/router/testing';
-import {LoginComponent} from "./components/login/login.component";
+import {LoginComponent} from './components/login/login.component';
+import {OauthHandlerComponent} from './components/oauth-handler/oauth-handler.component';
 
 const Login_ROUTES: Routes = [
   {path: '', component: LoginComponent, pathMatch: 'full'},
+  {path: 'oauth', component: OauthHandlerComponent},
 ];
 
 export const LoginRouting = RouterModule.forChild(Login_ROUTES);
--- src/app/site/site.routing.ts
+++ src/app/site/site.routing.ts
@@ -3,6 +3,7 @@ import {RouterTestingModule} from '@angular/router/testing';
 import {SiteComponent} from './site.component';
 import {LoginComponent} from './login/components/login/login.component';
 import {RegisterComponent} from "./register/components/register/register.component";
+import {OauthHandlerComponent} from './login/components/oauth-handler/oauth-handler.component';
 
 const Site_ROUTES: Routes = [
   {
@@ -11,6 +12,7 @@ const Site_ROUTES: Routes = [
       {path: 'home', loadChildren: 'app/site/home/home.module#HomeModule'},
       // {path: 'login', loadChildren: 'app/site/login/login.module#LoginModule'},
       {path: 'login', component: LoginComponent},
+      {path: 'login/oauth', component: OauthHandlerComponent},
       // {path: 'register', loadChildren: 'app/site/register/register.module#RegisterModule'},
       {path: 'register', component: RegisterComponent},
       {path: 'collection', loadChildren: 'app/site/collection/collection.module#CollectionModule'},
