d26c58697439dfa4c8d726ebdb94fd811041e46c
305-2 <sareh@bentoak.systems>
2018-02-21 19:13:27 +0330

    add paginator and delete fundtion to delete a page

--- src/app/shared/components/search-fields/search-fields.component.ts
+++ src/app/shared/components/search-fields/search-fields.component.ts
@@ -1,6 +1,10 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FormControl} from '@angular/forms';
+import {TargetEnum} from '../../enum/target.enum';
 
+enum ElementEnum {
+  isApp,
+}
 
 @Component({
   selector: 'app-search-fields',
@@ -10,19 +14,34 @@ import {FormControl} from '@angular/forms';
 export class SearchFieldsComponent implements OnInit {
 
   @Input() target = null;
+
+  @Input()
+  set initItems(value) {
+    this._initItems = value;
+    this.setInitSearchData();
+  }
+
+  get initItems() {
+    return this._initItems;
+  }
+
   @Output() searching = new EventEmitter<any>();
 
   targets = [];
   targetList = [];
   searchCtrl = new FormControl();
-
+  targetEnum = TargetEnum;
+  elementEnum = ElementEnum;
   phrase = null;
+  isApp = null;
+  _initItems: any = null;
+
+
 
   constructor() {
   }
 
   ngOnInit() {
-
     this.searchCtrl.valueChanges.debounceTime(500).subscribe(
       data => {
         this.phrase = data.trim() !== '' ? data.trim() : null;
@@ -50,8 +69,47 @@ export class SearchFieldsComponent implements OnInit {
     this.targets.forEach(el => {
       trg[el] = true;
     });
+    //
+    // const searchData = {
+    //   phrase: phrase,
+    //   options: {
+    //     target: trg,
+    //     is_app: this.isApp,
+    //     show_all: (this.target
+    //       && (phrase === null || phrase === '')
+    //       && this.isApp === null),
+    //   }
+    // };
+
 
     this.searching.emit(phrase);
+  }
+
+
+  changeState(element) {
+    switch (element) {
+      case this.elementEnum.isApp: {
+        if (this.isApp === null)
+          this.isApp = true;
+        else if (this.isApp === true)
+          this.isApp = false;
+        else if (this.isApp === false)
+          this.isApp = null;
+      }
+        break;
+    }
+
+    this.searchOnData();
+  }
+
+
+
+  setInitSearchData() {
+    if (this.initItems) {
+      this.searchCtrl.setValue(this.initItems.phrase ? this.initItems.phrase : null);
+      this.phrase = this.initItems.phrase ? this.initItems.phrase : null;
+      this.isApp = this.initItems.options.is_app ? this.initItems.options.is_app : null;
+    }
   }
 
 }
---
+++ src/app/shared/enum/target.enum.ts
@@ -0,0 +1,5 @@
+export enum TargetEnum {
+  page = 'page',
+  collection = 'collection',
+  product = 'product',
+}
