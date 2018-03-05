bff81e44fe9685253395c1c5bd0e58c279c796b3
305-2 <sareh@bentoak.systems>
2018-03-05 14:55:42 +0330

    add view of filtering panel and functionality/ fixing some bugs on it

--- src/app/shared/components/filtering-panel/filtering-panel.component.ts
+++ src/app/shared/components/filtering-panel/filtering-panel.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {filter_optionsEnum} from '../../enum/filter_options.enum';
 import {PlacementService} from '../../services/placement.service';
 
 @Component({
@@ -30,10 +29,8 @@ export class FilteringPanelComponent implements OnInit {
       values: ['#254867', '#101215', '#FFD72E', '#7CFF1B', '#FF7912', '#FFC3A8', '#FFC300', '#FFC344', '#FF00A8', '#1155A8', '#778F1B', '#FF7AD2', '#FFC322', '#5FC300', '#3FC344', '#FFAAA8', '#1003A8']
     }
   ];
-  selected_color = [];
-  selected_size = [];
   current_filter_state = [];
-  is_test = null;
+  clear_box = null;
 constructor(private placementService: PlacementService) {
   }
 
@@ -43,43 +40,10 @@ constructor(private placementService: PlacementService) {
     tempObj.name = el.name;
     this.current_filter_state.push(tempObj);
   })
-    console.log('****', this.current_filter_state);
-  }
-
-  getColor(name, colorCode) {
-    this.is_test = null;
-    if (this.selected_color.findIndex(c => c === colorCode) === -1)
-      this.selected_color.push(colorCode);
-    else {
-      const ind = this.selected_color.indexOf(colorCode);
-      if (ind > -1)
-        this.selected_color.splice(ind, 1);
-    }
-    this.current_filter_state.forEach(el => {
-      if (el.name === name) {
-        el.values = this.selected_color;
-      }
-    });
-  }
-
-  getSize(name, sizeNumber) {
-    this.is_test = null;
-    if (this.selected_size.findIndex(s => s === sizeNumber) === -1)
-      this.selected_size.push(sizeNumber);
-    else {
-      const ind = this.selected_size.indexOf(sizeNumber);
-      if (ind > -1)
-        this.selected_size.splice(ind, 1);
-    }
-    this.current_filter_state.forEach(el => {
-      if (el.name === name) {
-        el.values = this.selected_size;
-      }
-    });
+    console.log('filter_options : ', this.current_filter_state);
   }
 
   getValue(name, value) {
-    this.is_test = null;
     this.current_filter_state.forEach(el => {
       if (el.name === name) {
         if (el.values.length === 0 || el.values.findIndex(i => i === value) === -1)
@@ -90,51 +54,16 @@ constructor(private placementService: PlacementService) {
             el.values.splice(ind, 1);
         }
       }
-    })
-    console.log('===>>', this.current_filter_state);
+    });
+    this.clear_box = null;
+    // console.log('===>', this.current_filter_state);
   }
 
   clearFilters() {
     this.current_filter_state.forEach(el => {
       el.values = [];
     });
-    // this.filter_options.forEach(el => {
-    //   el.values = [];
-    // });
-    console.log(this.current_filter_state);
-    this.is_test = false;
+    this.clear_box = false;
+    // console.log('--->', this.current_filter_state);
   }
-  //
-  //   if (name === 'برند') {
-  //     if (this.selected_brand.findIndex(el => el === value) === -1)
-  //       this.selected_brand.push(value);
-  //     else {
-  //       const ind = this.selected_brand.indexOf(value);
-  //       if (ind > -1)
-  //         this.selected_brand.splice(ind, 1);
-  //     }
-  //
-  //     this.current_filter_state.forEach(el => {
-  //       if (el.name === name) {
-  //         el.values = this.selected_brand;
-  //       }
-  //     });
-  //   }
-  //   else if (name === 'نوع') {
-  //     if (this.selected_type.findIndex(el => el === value) === -1)
-  //       this.selected_type.push(value);
-  //     else {
-  //       const ind = this.selected_type.indexOf(value);
-  //       if (ind > -1)
-  //         this.selected_type.splice(ind, 1);
-  //     }
-  //
-  //     this.current_filter_state.forEach(el => {
-  //       if (el.name === name) {
-  //         el.values = this.selected_type;
-  //       }
-  //     });
-  //   }
-  //   console.log('===>>', this.current_filter_state);
-  // }
 }
