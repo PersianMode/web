0ac74102d73acec8f10787a45190b014a4016f95
AmS <alimirjahani7@gmail.com>
2018-03-08 14:49:07 +0330

    summary component

--- src/app/site/cart/cart.module.ts
+++ src/app/site/cart/cart.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import { SummaryComponent } from './components/summary/summary.component';
 import { CartItemsComponent } from './components/cart-items/cart-items.component';
 import {CartRouting} from './cart.routing';
+import { CartComponent } from './cart.component';
 
 @NgModule({
   declarations: [
     SummaryComponent,
     CartItemsComponent,
+    CartComponent,
   ],
   imports: [
     CartRouting,
--- src/app/site/cart/cart.routing.ts
+++ src/app/site/cart/cart.routing.ts
@@ -1,7 +1,10 @@
 import {RouterModule, Routes} from '@angular/router';
 import {RouterTestingModule} from '@angular/router/testing';
+import {CartComponent} from './cart.component';
 
 const Cart_ROUTES: Routes = [
+  {path: '', component: CartComponent, pathMatch: 'full'},
+
 ];
 
 export const CartRouting = RouterModule.forChild(Cart_ROUTES);
