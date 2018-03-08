041177110846aeaf74406d36b060f29bf8379cd7
AmS <alimirjahani7@gmail.com>
2018-03-08 18:33:12 +0330

    thursday finish

--- src/app/site/cart/cart.component.ts
+++ src/app/site/cart/cart.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {WINDOW} from '../../shared/services/window.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +7,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  isMobile = false;
+  products = [
+    {
+    instance_id: 14,
+    name: 'کایری ۳ مدل What The',
+    tags: ['کفش', 'بسکتبال', 'نوجوانان'],
+    price: 599000,
+    size: 6.5,
+    quantity: 3,
+    color: {
+      color_id: 101,
+      name: 'طلائی دانشگاهی/سیاه/زبرجدی تند/آبی نفتی'
+    },
+    thumbnail: '11.jpeg',
+    discount: '',
+    instances: [{
+      instance_id: 14,
+      size: 6.5,
+      quantity: [7],
+      discount: ''
+    }, {
+      instance_id: 15,
+      size: 7,
+      quantity: [5],
+      discount: ''
+    }, {
+      instance_id: 16,
+      size: 8,
+      qantity: [7],
+      discount: ''
+    }]
+  },
+    {
+      instance_id: 12,
+      name: 'mock data 2',
+      tags: ['کفش', 'بسکتبال', 'نوجوانان'],
+      price: 599000,
+      size: 6.5,
+      quantity: 3,
+      color: {
+        color_id: 101,
+        name: 'طلائی دانشگاهی/سیاه/زبرجدی تند/آبی نفتی'
+      },
+      thumbnail: '11.jpeg',
+      discount: '',
+      instances: [{
+        instance_id: 14,
+        size: 6.5,
+        quantity: [7],
+        discount: ''
+      }, {
+        instance_id: 15,
+        size: 7,
+        quantity: [5],
+        discount: ''
+      }, {
+        instance_id: 16,
+        size: 8,
+        qantity: [7],
+        discount: ''
+      }]
+    }];
 
-  constructor() { }
+  constructor(@Inject(WINDOW) private window) {
+    this.isMobile = this.isMobileCalc(this.window.innerWidth);
+  }
 
   ngOnInit() {
+  }
+
+  isMobileCalc(width): boolean {
+    return width < 960;
   }
 
 }
--- src/app/site/cart/components/cart-items/cart-items.component.ts
+++ src/app/site/cart/components/cart-items/cart-items.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {priceFormatter} from '../../../../shared/lib/priceFormatter';
 
 @Component({
@@ -7,49 +7,56 @@ import {priceFormatter} from '../../../../shared/lib/priceFormatter';
   styleUrls: ['./cart-items.component.css']
 })
 export class CartItemsComponent implements OnInit {
-  product: any = {
-    instance_id: 14,
-    name: 'کایری ۳ مدل What The',
-    tags: ['کفش', 'بسکتبال', 'نوجوانان'],
-    price: 599000,
-    size: 6.5,
-    quantity : 3,
-    color : {
-      color_id : 101,
-      name: 'طلائی دانشگاهی/سیاه/زبرجدی تند/آبی نفتی'
-    },
-    thumbnail : '11.jpeg',
-    discount : '',
-    instances : [{
-      instance_id : 14,
-      size : 6.5,
-      quantity : [7],
-      discount : ''
-    }, {
-      instance_id : 15,
-      size : 7,
-      quantity : [5],
-      discount : ''
-    }, {
-      instance_id : 16,
-      size : 8,
-      qantity : [7],
-      discount : ''
-    }]
-  };
+  @Input() private product = null;
 
   total_price = null;
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit() {
+    if (this.product == null) {
+      this.product = {
+        instance_id: 14,
+        name: 'default value',
+        tags: ['saf', 'dasf', 'safaf'],
+        price: 599000,
+        size: 6.5,
+        quantity: 3,
+        color: {
+          color_id: 101,
+          name: 'طلائی دانشگاهی/سیاه/زبرجدی تند/آبی نفتی'
+        },
+        thumbnail: '11.jpeg',
+        discount: '',
+        instances: [{
+          instance_id: 14,
+          size: 6.5,
+          quantity: [7],
+          discount: ''
+        }, {
+          instance_id: 15,
+          size: 7,
+          quantity: [5],
+          discount: ''
+        }, {
+          instance_id: 16,
+          size: 8,
+          qantity: [7],
+          discount: ''
+        }]
+      };
+    }
     this.total_price = this.product.quantity * this.product.price;
     this.total_price = priceFormatter(this.total_price);
     this.product.price = priceFormatter(this.product.price);
     this.product.size = this.product.size.toLocaleString('fa');
     this.product.quantity = this.product.quantity.toLocaleString('fa');
   }
+
   deleteProduct() {
   }
+
   editOrder() {
   }
 }
