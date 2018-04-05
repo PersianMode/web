ffba7e601c428189450f746ce61e6c8373ddb01b
AmS <alimirjahani7@gmail.com>
2018-04-05 11:23:33 +0430

    Merge branch 'master' of https://github.com/PersianMode/web into checkout-page
    # Conflicts:
    #	src/app/site/checkout/checkout.module.ts

--- src/app/site/checkout/address-table/address-table.component.ts
+++ src/app/site/checkout/address-table/address-table.component.ts
@@ -1,5 +1,4 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {WINDOW} from '../../../shared/services/window.service';
 import {HttpService} from '../../../shared/services/http.service';
 import {UpsertAddressComponent} from '../../../shared/components/upsert-address/upsert-address.component';
 import {MatDialog} from '@angular/material';
@@ -15,6 +14,25 @@ export class AddressTableComponent implements OnInit {
   withDelivery = true;
   selectedCustomerAddresses = -1;
   selectedWareHouseAddresses = -1;
+
+  address = {
+    ostan: 'البرز',
+    city: 'کرج',
+    street: 'دربند',
+    no: 14,
+    unit: 1,
+    postal_code: 1044940912,
+    loc: {
+      long: 50.817191,
+      lat: 51.427251,
+    },
+    recipient_name: 'علی',
+    recipient_surname: 'علوی',
+    recipient_mobile_no: '09121212121',
+    recipient_national_id: '06423442',
+    recipient_title: 'm',
+    district: 'خیابان سوم'
+  };
 
   // addresses = [
   //   {
@@ -123,26 +141,10 @@ export class AddressTableComponent implements OnInit {
     return (+a).toLocaleString('fa', {useGrouping: false});
   }
 
-  constructor(private dialog: MatDialog, private authService: AuthService, private httpService: HttpService) { }
+  constructor(private dialog: MatDialog, private authService: AuthService, private httpService: HttpService) {
+  }
 
-  address = {
-    ostan: 'البرز',
-    city: 'کرج',
-    street: 'دربند',
-    no: 14,
-    unit: 1,
-    postal_code: 1044940912,
-    loc: {
-      long: 50.817191,
-      lat: 51.427251,
-    },
-    recipient_name: 'علی',
-    recipient_surname: 'علوی'
-    recipient_mobile_no: '09121212121',
-    recipient_national_id: '06423442',
-    recipient_title: 'm',
-    district: 'خیابان سوم'
-  };
+
   ngOnInit() {
     this.getCustomerAddresses();
     this.getWareHouseAddresses();
@@ -154,7 +156,7 @@ export class AddressTableComponent implements OnInit {
     const tempAddressId = (id || id === 0) ? id + 1 : null;
     const tempAddress = (id || id === 0) ? this.addresses[id] : null;
     const partEdit = !!(id || id === 0);
-z    const rmDialog = this.dialog.open(UpsertAddressComponent, {
+    const rmDialog = this.dialog.open(UpsertAddressComponent, {
       width: '600px',
       data: {
         addressId: tempAddressId,
@@ -174,25 +176,25 @@ z    const rmDialog = this.dialog.open(UpsertAddressComponent, {
       this.addresses = this.customerAddresses;
     else
       this.addresses = this.wareHouseAddresses;
+    (data) => {
+      if (data) {
+        console.log('*****', data);
+        this.httpService.post('user/address', {
+          username: this.authService.userDetails.username,
+          body: data,
+        }).subscribe(
           (data) => {
-        if (data) {
-          console.log('*****', data);
-          this.httpService.post('user/address', {
-            username: this.authService.userDetails.username,
-            body: data,
-          }).subscribe(
-            (data) => {
-              console.log('sucsess');
-            },
-            (err) => {
-              console.error('Cannot set address');
-            }
-          );
-        }
-      },
+            console.log('sucsess');
+          },
+          (err) => {
+            console.error('Cannot set address');
+          }
+        );
+      }
+    },
       (err) => {
         console.error('Error in dialog: ', err);
       }
-    );
-  }
+  );
+}
 }
