2cf162a07a1ea9b40631768e549809a1fc7b28b0
Farhad Mohammadali <mafphp@gmail.com>
2018-04-07 17:43:35 +0430

    001 - added directory and files for order-list

--- src/app/admin/admin.routing.ts
+++ src/app/admin/admin.routing.ts
@@ -10,7 +10,7 @@ const Admin_ROUTES: Routes = [
     {path: 'products', loadChildren: 'app/admin/product/product.module#ProductModule', canActivate: [AdminAuthGuard]},
     {path: 'pages', loadChildren: 'app/admin/page/page.module#PageModule', canActivate: [AdminAuthGuard]},
     {path: 'uploads', loadChildren: 'app/admin/upload/upload.module#UploadModule', canActivate: [AdminAuthGuard]},
-    {path: 'orders', loadChildren: 'app/admin/order/order.module#OrderModule', canActivate: [AdminAuthGuard]},
+    {path: 'orders', loadChildren: 'app/admin/order/order.module#OrderModule'},
   ]},
 ];
 
--- src/app/admin/order/order.module.ts
+++ src/app/admin/order/order.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {OrderComponent} from './order.component';
+import {OrderListComponent} from './order-list/order-list.component';
 import {OrderRouting} from './order.routing';
 import {SharedModule} from '../../shared/shared.module';
 
 @NgModule({
   declarations: [
     OrderComponent,
+    OrderListComponent
   ],
   imports: [
     OrderRouting,
