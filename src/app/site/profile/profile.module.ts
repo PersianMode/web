dc1191dcb4f110a46910c9bc89199bdad59992a1
305-2 <sareh@bentoak.systems>
2018-04-07 17:24:49 +0430

    add profile module

--- src/app/site/profile/profile.module.ts
+++ src/app/site/profile/profile.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BasicInfoComponent } from './components/basic-info/basic-info.component';
 import {ProfileRouting} from './profile.routing';
+import { AddressesComponent } from './components/addresses/addresses.component';
+import { OrdersComponent } from './components/orders/orders.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ProfileRouting
   ],
-  declarations: [BasicInfoComponent]
+  declarations: [BasicInfoComponent, AddressesComponent, OrdersComponent]
 })
 export class ProfileModule { }
