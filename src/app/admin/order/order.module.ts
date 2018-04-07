a1905348f3060b3fb193b163e57725d2e0a6e4d7
Farhad Mohammadali <mafphp@gmail.com>
2018-04-07 18:48:59 +0430

    002 - mat-table module and another module added

--- src/app/admin/order/order.module.ts
+++ src/app/admin/order/order.module.ts
@@ -2,19 +2,25 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {OrderComponent} from './order.component';
 import {OrderListComponent} from './order-list/order-list.component';
+import {MatPaginatorModule, MatSortModule} from '@angular/material';
+import {MatTableModule} from '@angular/material/table';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {OrderRouting} from './order.routing';
 import {SharedModule} from '../../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    OrderComponent,
-    OrderListComponent
-  ],
   imports: [
     OrderRouting,
     CommonModule,
     SharedModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    MatTableModule
+  ],
+  declarations: [
+    OrderComponent,
+    OrderListComponent
   ],
 })
-export class OrderModule {
-}
+export class OrderModule {}
