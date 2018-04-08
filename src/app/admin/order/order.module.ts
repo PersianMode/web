879f703a84d56f92ae911efc86eeab1728ed254d
Farhad Mohammadali <mafphp@gmail.com>
2018-04-08 23:15:36 +0430

    order-table work with data mock

--- src/app/admin/order/order.module.ts
+++ src/app/admin/order/order.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {OrderComponent} from './order.component';
-import {OrderListComponent} from './order-list/order-list.component';
-import {MatPaginatorModule, MatSortModule} from '@angular/material';
-import {MatTableModule} from '@angular/material/table';
+import {OrderTableComponent} from './components/order-table/order-table.component';
+import {MatPaginatorModule, MatTableModule, MatSortModule} from '@angular/material';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {OrderRouting} from './order.routing';
 import {SharedModule} from '../../shared/shared.module';
+import { OrderService } from './order.service';
 
 @NgModule({
   imports: [
@@ -20,7 +20,8 @@ import {SharedModule} from '../../shared/shared.module';
   ],
   declarations: [
     OrderComponent,
-    OrderListComponent
+    OrderTableComponent
   ],
+  providers: [OrderService]
 })
 export class OrderModule {}
