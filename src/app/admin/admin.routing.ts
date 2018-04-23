caaee6252c0d7e529f9809db2f2bc507307970e4
305-2 <sareh@bentoak.systems>
2018-04-23 17:08:31 +0430

    remove order admin components

--- src/app/admin/admin.routing.ts
+++ src/app/admin/admin.routing.ts
@@ -11,7 +11,7 @@ const Admin_ROUTES: Routes = [
     {path: 'products', loadChildren: 'app/admin/product/product.module#ProductModule', canActivate: [AdminAuthGuard]},
     {path: 'pages', loadChildren: 'app/admin/page/page.module#PageModule', canActivate: [AdminAuthGuard]},
     {path: 'uploads', loadChildren: 'app/admin/upload/upload.module#UploadModule', canActivate: [AdminAuthGuard]},
-    {path: 'orders', loadChildren: 'app/admin/order/order.module#OrderModule'},
+    {path: 'orders', loadChildren: 'app/admin/order/order.module#OrderModule', canActivate: [AdminAuthGuard]},
   ]},
 ];
 
