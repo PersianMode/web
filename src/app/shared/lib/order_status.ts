79eba12ecd3efc223dfb81a743299e15466edb18
eabasir@gmail.com <eabasir@gmail.com>
2018-04-24 21:53:33 +0330

    change order statuses

--- src/app/shared/lib/order_status.ts
+++ src/app/shared/lib/order_status.ts
@@ -2,15 +2,8 @@ import {STATUS} from '../enum/status.enum';
 
 export let OrderStatus: any[] = [
   {name: 'تایید پرداخت', status: STATUS.default},
-  {name: 'ارجاع به فروشگاه', status: STATUS.SMAssignToWarehouse},
-  {name: 'بازگشت هزینه', status: STATUS.SMRefund},
-  {name: 'عدم تایید فروشگاه', status: STATUS.SCDeclined},
-  {name: 'ارسال از فروشگاه به مرکز', status: STATUS.SCSentToCentral},
+  {name: 'انتظار تایید انبار آنلاین', status: STATUS.WaiteForOnlineWarehouse},
+  {name: 'انتظار صدور فاکتور', status: STATUS.WaitForInvoice},
   {name: 'صدور فاکتور', status: STATUS.Invoice},
   {name: 'آماده تحویل', status: STATUS.ReadyToDeliver},
-  {name: 'در حال ارسال', status: STATUS.OnDelivery},
-  {name: 'ارسال موفق', status: STATUS.DeliverySuccess},
-  {name: 'ارسال ناموفق', status: STATUS.DeliveryFailed},
-  {name: 'لغو', status: STATUS.Cancel},
-  {name: 'استرداد', status: STATUS.Return},
 ];
