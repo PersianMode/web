72a95a3f6c4887281dc216362e4de7683e732045
imans777 <iman.s_sani@yahoo.com>
2018-04-25 15:24:37 +0430

    remove extra comments

--- src/app/admin/page/components/page-basic-info/page-basic-info.component.ts
+++ src/app/admin/page/components/page-basic-info/page-basic-info.component.ts
@@ -301,35 +301,4 @@ export class PageBasicInfoComponent implements OnInit {
     }
   }
 
-  imageClicked() {
-    // function image() {
-    //   let fileInput = this.container.querySelector('input.ql-image[type=file]');
-    //   if (fileInput == null) {
-    //     fileInput = document.createElement('input');
-    //     fileInput.setAttribute('type', 'file');
-    //     fileInput.setAttribute('accept', 'image/png, image/gif, image/jpeg, image/bmp, image/x-icon');
-    //     fileInput.classList.add('ql-image');
-    //     fileInput.addEventListener('change', () => {
-    //       if (fileInput.files != null && fileInput.files[0] != null) {
-    //         let reader = new FileReader();
-    //         reader.onload = (e) => {
-    //           let range = this.quill.getSelection(true);
-    //           this.quill.updateContents(new Delta()
-    //               .retain(range.index)
-    //               .delete(range.length)
-    //               .insert({ image: e.target.result })
-    //             , Emitter.sources.USER);
-    //           this.quill.setSelection(range.index + 1, Emitter.sources.SILENT);
-    //           fileInput.value = "";
-    //         }
-    //         reader.readAsDataURL(fileInput.files[0]);
-    //       }
-    //     });
-    //     this.container.appendChild(fileInput);
-    //   }
-    //   fileInput.click();
-    // }
-
-    console.log('of course this should be executed');
-  }
 }
