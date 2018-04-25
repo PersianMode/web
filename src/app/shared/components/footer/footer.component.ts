7bffa88049a2a0fb921ed81ba89197e96ec294b1
arhariri <ali.71hariri@gmail.com>
2018-04-25 12:21:33 +0430

    make footer to be responsive

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -61,8 +61,6 @@ export class FooterComponent implements OnInit {
         return 0;
       });
     });
-
-    console.log(this.footerSiteLinksItems);
   }
 
   setFooterSocialLinks(list) {
@@ -75,8 +73,6 @@ export class FooterComponent implements OnInit {
     }).map(el => {
       return {text: el.info.text + ' icons', href: el.info.href};
     });
-
-    console.log('social links: ', this.footerSocialNetworkItems);
   }
 
   getKeyList(list) {
