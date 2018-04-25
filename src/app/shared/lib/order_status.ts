f120bbac46aaa9a2913bd0bff936062da751c1bc
305-2 <sareh@bentoak.systems>
2018-04-25 13:29:58 +0430

    add title key to order-status.ts file

--- src/app/shared/lib/order_status.ts
+++ src/app/shared/lib/order_status.ts
@@ -1,16 +1,16 @@
 import {STATUS} from '../enum/status.enum';
 
 export let OrderStatus: any[] = [
-  {name: 'تایید پرداخت', status: STATUS.default},
-  {name: 'ارجاع به فروشگاه', status: STATUS.SMAssignToWarehouse},
-  {name: 'بازگشت هزینه', status: STATUS.SMRefund},
-  {name: 'عدم تایید فروشگاه', status: STATUS.SCDeclined},
-  {name: 'ارسال از فروشگاه به مرکز', status: STATUS.SCSentToCentral},
-  {name: 'صدور فاکتور', status: STATUS.Invoice},
-  {name: 'آماده تحویل', status: STATUS.ReadyToDeliver},
-  {name: 'در حال ارسال', status: STATUS.OnDelivery},
-  {name: 'ارسال موفق', status: STATUS.DeliverySuccess},
-  {name: 'ارسال ناموفق', status: STATUS.DeliveryFailed},
-  {name: 'لغو', status: STATUS.Cancel},
-  {name: 'استرداد', status: STATUS.Return},
+  {name: 'تایید پرداخت', title: 'درحال پردازش', status: STATUS.default},
+  {name: 'ارجاع به فروشگاه', title: 'درحال پردازش', status: STATUS.SMAssignToWarehouse},
+  {name: 'بازگشت هزینه', title: 'بازگشت هزینه', status: STATUS.SMRefund},
+  {name: 'عدم تایید فروشگاه', title: 'درحال پردازش', status: STATUS.SCDeclined},
+  {name: 'ارسال از فروشگاه به مرکز', title: 'درحال پردازش', status: STATUS.SCSentToCentral},
+  {name: 'صدور فاکتور', title: 'درحال پردازش', status: STATUS.Invoice},
+  {name: 'آماده تحویل', title: 'درحال پردازش', status: STATUS.ReadyToDeliver},
+  {name: 'در حال ارسال', title: 'درحال پردازش', status: STATUS.OnDelivery},
+  {name: 'ارسال موفق', title: 'سفارش تحویل شده', status: STATUS.DeliverySuccess},
+  {name: 'ارسال ناموفق', title: 'درحال پردازش', status: STATUS.DeliveryFailed},
+  {name: 'لغو', title: 'سفارش لغو شده', status: STATUS.Cancel},
+  {name: 'استرداد', title: 'بازگشت کالا', status: STATUS.Return},
 ];
