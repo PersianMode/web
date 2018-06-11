4beb5f91dda0095bacccbc19d77aa1b5b61113b8
Farhad Mohammadali <mafphp@gmail.com>
2018-06-11 11:02:04 +0430

    delete status and receiverId from client

--- src/app/site/profile/components/order-return/order-return.component.ts
+++ src/app/site/profile/components/order-return/order-return.component.ts
@@ -2,13 +2,11 @@ import { Component, OnInit, Inject, Input, Output, EventEmitter } from '@angular
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import * as moment from 'moment';
 import { HttpService } from 'app/shared/services/http.service';
-import {STATUS} from '../../../../shared/enum/status.enum';
 import { Location } from '@angular/common';
 import { ProfileOrderService } from '../../../../shared/services/profile-order.service';
 
 export interface ITicket {
   receiver_id: string;
-  status: number;
   desc: {
     periodTime: string,
     addressId: string,
@@ -28,7 +26,6 @@ export class OrderReturnComponent implements OnInit {
   orderObject: any;
   ticket: ITicket = {
     receiver_id: '',
-    status: 14,
     desc: {
       periodTime: '',
       addressId: '',
@@ -39,21 +36,16 @@ export class OrderReturnComponent implements OnInit {
   dateObject;
   timeReturn: string;
   returnTimes = [{value: '10-18', time: '10-18', checked: true}, {value: '18-22', time: '18-22', checked: false}];
-  // private dialogRef: MatDialogRef<OrderReturnComponent>,
-  //   @Inject(MAT_DIALOG_DATA) public data: any,
-  constructor( private httpService: HttpService,private location: Location, 
+  constructor( private httpService: HttpService, private location: Location,
     private profileOrderService: ProfileOrderService) { }
 
   ngOnInit() {
     this.orderObject = this.profileOrderService.orderData;
-    console.log(this.profileOrderService.orderData);
     this.httpService.get('warehouse/hup').subscribe(res => {
-      this.ticket.status = STATUS.Return;
       this.ticket.receiver_id = res._id;
     });
   }
   cancelReturnOrder() {
-    // TODO: guard is needed if any fields have been changed!
     if (this.isNotMobile) {
       this.closeDialog.emit(false);
     } else {
