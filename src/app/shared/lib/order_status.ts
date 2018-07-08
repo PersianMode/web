074481afb07cc1401c19305fe4beb5f04bdbbf25
Farhad Mohammadali <mafphp@gmail.com>
2018-07-08 13:58:56 +0430

    status order changed

--- src/app/shared/lib/order_status.ts
+++ src/app/shared/lib/order_status.ts
@@ -4,13 +4,11 @@ export let OrderStatus: any[] = [
   {name: 'تایید پرداخت', title: 'تایید پرداخت', status: STATUS.default},
   {name: 'انتظار تایید انبار آنلاین', title: 'در حال پردازش', status: STATUS.WaitForOnlineWarehouse},
   {name: 'انتظار تجمیع', title: 'در حال پردازش', status: STATUS.WaitForAggregation},
-  {name: 'آماده انتقال داخلی', title: 'در حال پردازش', status: STATUS.ReadyForInternalDelivery},
-  {name: 'در حال انتقال داخلی', title: 'در حال پردازش', status: STATUS.OnInternalDelivery},
-  {name: 'در حال آماده صدور فاکتور', title: 'در حال پردازش', status: STATUS.ReadyForInvoice},
-  {name: 'آماده صدور فاکتور', title: 'در حال پردازش', status: STATUS.WaitForInvoice},
-  {name: 'فاکتور صادر شده', title: 'در حال پردازش', status: STATUS.InvoiceVerified},
-  {name: 'آماده تحویل', title: 'آماده ارسال', status: STATUS.ReadyToDeliver},
-  {name: 'تعیین پیک', title: 'ارسال تایید شده', status: STATUS.DeliverySet},
+  {name: 'آماده صدور فاکتور', title: 'در حال پردازش', status: STATUS.ReadyForInvoice},
+  {name: 'در انتظار صدور فاکتور', title: 'در حال پردازش', status: STATUS.WaitForInvoice},
+  {name: 'تایید فاکتور', title: 'در حال پردازش', status: STATUS.InvoiceVerified},
+  {name: 'آماده تحویل', title: 'آماده تحویل', status: STATUS.ReadyToDeliver},
+  {name: 'تعیین پیک', title: 'آماده تحویل', status: STATUS.DeliverySet},
   {name: 'در حال ارسال', title: 'در حال ارسال', status: STATUS.OnDelivery},
   {name: 'تحویل شده', title: 'تحویل شده', status: STATUS.Delivered},
   {name: 'ناموجود', title: 'ناموجود', status: STATUS.NotExists},
@@ -18,3 +16,4 @@ export let OrderStatus: any[] = [
   {name: 'بازگشت کالا', title: 'بازگشت کالا', status: STATUS.Return},
   {name: 'لغو سفارش', title: 'لغو سفارش', status: STATUS.Cancel},
 ];
+
