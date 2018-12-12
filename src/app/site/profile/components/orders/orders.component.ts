a143801bdfe5279865fd6a499d30fbbe2316696f
imans777 <iman.s_sani@yahoo.com>
2018-12-12 19:41:55 +0330

    remove redundancy

--- src/app/site/profile/components/orders/orders.component.ts
+++ src/app/site/profile/components/orders/orders.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {ProfileOrderService} from '../../../../shared/services/profile-order.service';
 import {Router} from '@angular/router';
 import {dateFormatter} from '../../../../shared/lib/dateFormatter';
@@ -8,10 +8,6 @@ import {DialogEnum} from '../../../../shared/enum/dialog.components.enum';
 import {MatDialog} from '@angular/material';
 import {HttpService} from '../../../../shared/services/http.service';
 import {ProgressService} from '../../../../shared/services/progress.service';
-import * as moment from 'jalali-moment';
-import { OrderReturnComponent } from '../order-return/order-return.component';
-import { OrderCancelComponent } from '../order-cancel/order-cancel.component';
-
 
 
 @Component({
@@ -25,18 +21,20 @@ export class OrdersComponent implements OnInit, OnDestroy {
   displayedColumns = ['col_no', 'date', 'order_lines', 'total_amount', 'discount', 'used_point', 'address', 'view_details'];
   isMobile = false;
   selectedOrder;
+
   constructor(private profileOrderService: ProfileOrderService, private router: Router,
               private responsiveService: ResponsiveService,
               private dialog: MatDialog, protected httpService: HttpService,
               protected progressService: ProgressService) {
     this.isMobile = this.responsiveService.isMobile;
   }
+
   ngOnInit() {
     this.profileOrderService.orderArray.subscribe(result => {
       if (!result.length)
         return;
       this.profileOrder = result;
-            this.profileOrder.forEach(el => [el.jalali_date, el.time] = dateFormatter(el.order_time));
+      this.profileOrder.forEach(el => [el.jalali_date, el.time] = dateFormatter(el.order_time));
     });
     this.isMobile = this.responsiveService.isMobile;
     this.profileOrderService.getAllOrders();
@@ -48,27 +46,26 @@ export class OrdersComponent implements OnInit, OnDestroy {
       return a;
     return (+a).toLocaleString('fa', {useGrouping: isPrice});
   }
-   goToOrderLines(orderId) {
-     this.profileOrderService.getAllOrders();
 
-     this.selectedOrder = {
-       orderId: orderId,
-       dialog_order: this.profileOrder.find(el => el._id === orderId),
-     };
-     this.profileOrderService.orderData = this.selectedOrder;
-     if (this.responsiveService.isMobile) {
-       this.router.navigate([`/profile/orderlines`]);
-     } else {
-       const rmDialog = this.dialog.open(GenDialogComponent, {
-         width: '700px',
-         data: {
-           componentName: DialogEnum.orderLinesComponent,
-         }
-       }).afterClosed().subscribe(res => {
-        this.profileOrderService.getAllOrders();
-       });
-     }
-   };
+  goToOrderLines(orderId) {
+    this.profileOrderService.getAllOrders();
+
+    this.selectedOrder = {
+      orderId: orderId,
+      dialog_order: this.profileOrder.find(el => el._id === orderId),
+    };
+    this.profileOrderService.orderData = this.selectedOrder;
+    if (this.responsiveService.isMobile) {
+      this.router.navigate([`/profile/orderlines`]);
+    } else {
+      const rmDialog = this.dialog.open(GenDialogComponent, {
+        width: '700px',
+        data: {
+          componentName: DialogEnum.orderLinesComponent,
+        }
+      });
+    }
+  };
 
   ngOnDestroy() {
   };
