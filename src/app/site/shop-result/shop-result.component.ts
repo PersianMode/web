f9149c2d355a42a840a84142cc030a2e20c265d2
305-2 <sareh@bentoak.systems>
2018-12-23 19:08:47 +0330

    check transaction result from bank

--- src/app/site/shop-result/shop-result.component.ts
+++ src/app/site/shop-result/shop-result.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {HttpService} from '../../shared/services/http.service';
 import {HttpClient} from '@angular/common/http';
+import {dateFormatter} from '../../shared/lib/dateFormatter';
+import {SpinnerService} from '../../shared/services/spinner.service';
 
 @Component({
   selector: 'app-shop-result',
@@ -10,37 +12,45 @@ import {HttpClient} from '@angular/common/http';
 })
 export class ShopResultComponent implements OnInit {
   bankReferData: any = null;
+  resultObj: any = null;
+  jalali_date = [];
+  spinnerEnabled = true;
 
   constructor(private router: Router, private route: ActivatedRoute,
               private httpService: HttpService,
-              private http: HttpClient) {
+              private spinnerService: SpinnerService) {
   }
 
   ngOnInit() {
+    this.spinnerService.enable();
     this.route.queryParams.subscribe(params => {
-      console.log(params);
       this.bankReferData = params;
     });
+    const shopData = {
+      merchantCode: 4480470,
+      terminalCode: 1660557,
+      tref: this.bankReferData.tref,
+      invoiceNumber: this.bankReferData.iN,
+      invoiceDate: this.bankReferData.iD,
+    };
+    return new Promise((resolve, reject) => {
+      this.httpService.post('payResult', shopData)
+        .subscribe(res => {
+            this.resultObj = res.resultObj;
+            this.jalali_date = dateFormatter(this.resultObj.invoiceDate[0]);
+            this.spinnerService.disable();
+            this.spinnerEnabled = false;
+            resolve(res);
+          },
+          err => {
+            reject();
+          });
+    });
+  }
 
-
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://pep.shaparak.ir/CheckTransactionResult.aspx', true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    xhr.send(this.bankReferData.tref.toString());
-
-    // return new Promise((resolve, reject) => {
-    //   this.http.post('https://pep.shaparak.ir/CheckTransactionResult.aspx', this.bankReferData.tref, {
-    //     headers: {
-    //       'Content-Type': 'application/json; charset=UTF-8',
-    //     }
-    //   })
-    //     .subscribe(
-    //       (info: any) => {
-    //         console.log(info);
-    //       }, err => {
-    //         console.log('err: ', err);
-    //       }
-    //     );
-    // });
+  makePersianNumber(a: string) {
+    if (isNaN((+a)))
+      return a;
+    return (+a).toLocaleString('fa', {useGrouping: false});
   }
 }
