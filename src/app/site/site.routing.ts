c9e5231da60af7f9be3e587534dad77423062f71
305-2 <sareh@bentoak.systems>
2019-01-12 16:56:58 +0330

    no mock data used, error handling improved, request to data base reduced

--- src/app/site/site.routing.ts
+++ src/app/site/site.routing.ts
@@ -9,30 +9,30 @@ import {UpsertAddressComponent} from '../shared/components/upsert-address/upsert
 import {AuthGuard} from './auth.guard';
 import {OrderLinesComponent} from './profile/components/order-lines/order-lines.component';
 import {ForgotPasswordComponent} from './login/components/forgot-password/forgot-password.component';
-import { OrderReturnComponent } from './profile/components/order-return/order-return.component';
+import {OrderReturnComponent} from './profile/components/order-return/order-return.component';
 import {ShopResultComponent} from './shop-result/shop-result.component';
 
 const Site_ROUTES: Routes = [
   {
     path: '', component: SiteComponent, children: [
-    {path: '', redirectTo: 'home', pathMatch: 'full'},
-    {path: 'home', loadChildren: 'app/site/home/home.module#HomeModule'},
-    {path: 'login/oauth/:link', component: OauthHandlerComponent},
-    {path: 'login/oauth', component: OauthHandlerComponent},
-    {path: 'login', component: LoginComponent},
-    {path: 'checkout', loadChildren: 'app/site/checkout/checkout.module#CheckoutModule'},
-    {path: 'register', component: RegisterComponent},
-    {path: 'forgot/password', component: ForgotPasswordComponent},
-    {path: 'collection', loadChildren: 'app/site/collection/collection.module#CollectionModule'},
-    {path: 'page', loadChildren: 'app/site/page/page.module#PageModule'},
-    {path: 'cart', loadChildren: 'app/site/cart/cart.module#CartModule'},
-    {path: 'product', loadChildren: 'app/site/product/product.module#ProductModule'},
-    {path: 'checkout/address', component: UpsertAddressComponent},
-    {path: 'profile', loadChildren: 'app/site/profile/profile.module#ProfileModule', canActivate: [AuthGuard]},
-    {path: 'profile/orderlines', component: OrderLinesComponent},
-    {path: 'profile/orderlines/return', component: OrderReturnComponent},
-    {path: 'shopResult/:tref/:iN/:iD', component: ShopResultComponent},
-  ]
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {path: 'home', loadChildren: 'app/site/home/home.module#HomeModule'},
+      {path: 'login/oauth/:link', component: OauthHandlerComponent},
+      {path: 'login/oauth', component: OauthHandlerComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'checkout', loadChildren: 'app/site/checkout/checkout.module#CheckoutModule'},
+      {path: 'register', component: RegisterComponent},
+      {path: 'forgot/password', component: ForgotPasswordComponent},
+      {path: 'collection', loadChildren: 'app/site/collection/collection.module#CollectionModule'},
+      {path: 'page', loadChildren: 'app/site/page/page.module#PageModule'},
+      {path: 'cart', loadChildren: 'app/site/cart/cart.module#CartModule'},
+      {path: 'product', loadChildren: 'app/site/product/product.module#ProductModule'},
+      {path: 'checkout/address', component: UpsertAddressComponent},
+      {path: 'profile', loadChildren: 'app/site/profile/profile.module#ProfileModule', canActivate: [AuthGuard]},
+      {path: 'profile/orderlines', component: OrderLinesComponent},
+      {path: 'profile/orderlines/return', component: OrderReturnComponent},
+      {path: 'shopResult', component: ShopResultComponent},
+    ]
   }
 ];
 
