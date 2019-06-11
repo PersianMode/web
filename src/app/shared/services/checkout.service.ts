08ee393ec4b9310c05c484374d043c71159c871e
eabasir@gmail.com <eabasir@gmail.com>
2019-06-11 16:00:26 +0430

    Update checkout.service.ts

--- src/app/shared/services/checkout.service.ts
+++ src/app/shared/services/checkout.service.ts
@@ -319,7 +319,6 @@ export class CheckoutService {
   }
 
   readPayResult(bankData) {
-    console.log('-----> ', bankData);
     return new Promise((resolve, reject) => {
       this.httpService.post('payResult', bankData).subscribe(
         (data) => {
