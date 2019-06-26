967bc471499d37ed168e041b9906d2c8af183bde
Amin Azarbadegan <amin@azarbadegan.net>
2019-06-26 19:37:34 +0430

    Fixing size sorter to work with objects as well as simple data types

--- src/app/shared/lib/sizeSorter.ts
+++ src/app/shared/lib/sizeSorter.ts
@@ -1,14 +1,23 @@
 const freeSizes = ['XS', 'S', 'M', 'L', 'XL', '2XL', 'MISC'];
 export const sizeSorter = (a, b) => {
-  const ai = freeSizes.indexOf(('' + a).toUpperCase());
-  const bi = freeSizes.indexOf(('' + b).toUpperCase());
+  let av = a, bv = b;
+  if (a.value) {
+    av = a.value;
+  }
+
+  if (b.value) {
+    bv = b.value;
+  }
+
+  const ai = freeSizes.indexOf(('' + av).toUpperCase());
+  const bi = freeSizes.indexOf(('' + bv).toUpperCase());
   if (ai !== -1 && bi !== -1) {
     return ai - bi;
   } else if (ai !== -1 ) {
     return 1;
   } else if (bi !== -1) {
     return -1;
-  } else if (+a && + b) {
-    return a - b;
-  } else return '' + a < '' + b ? -1 : 1;
+  } else if (+av && + bv) {
+    return av - bv;
+  } else return '' + av < '' + bv ? -1 : 1;
 };
